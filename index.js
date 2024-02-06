ef230a5bdc1efd71c7df514b0f075c2a9e98e539
AML <a.lobo@castsoftware.com>
2024-02-06 07:55:11 +0530

    added dice rolling animations

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
 var randomNumber1 = Math.ceil(Math.random() * 6);
 var randomNumber2 = Math.ceil(Math.random() * 6);
-var resultTitle = "Let's see who wins!"; 
+var resultTitle = "Let's see who wins!";
 
 var rollButton = document.getElementById('rollButton');
 rollButton.addEventListener('click', function () {
-    randomNumber1 = Math.ceil(Math.random() * 6);
-    randomNumber2 = Math.ceil(Math.random() * 6);
+    var dice1 = document.getElementById("dice1");
+    var dice2 = document.getElementById("dice2");
 
-    document.getElementById("dice1").setAttribute("src", "./images/dice" + randomNumber1 + ".png");
-    document.getElementById("dice2").setAttribute("src", "./images/dice" + randomNumber2 + ".png");
+    dice1.classList.add("rolling");
+    dice2.classList.add("rolling");
 
-    if (randomNumber1 > randomNumber2)
-        resultTitle = 'Player 1 wins!';
-    else if (randomNumber2 > randomNumber1)
-        resultTitle = 'Player 2 wins!';
-    else
-        resultTitle = 'It\'s a draw! Roll again.';
+    var duration = Math.random() * 500 + 500; 
 
-    
-    document.getElementById("heading").textContent = resultTitle;
+    setTimeout(function () {
+        randomNumber1 = Math.ceil(Math.random() * 6);
+        randomNumber2 = Math.ceil(Math.random() * 6);
+
+        dice1.setAttribute("src", "./images/dice" + randomNumber1 + ".png");
+        dice2.setAttribute("src", "./images/dice" + randomNumber2 + ".png");
+
+        if (randomNumber1 > randomNumber2)
+            resultTitle = 'Player 1 wins!';
+        else if (randomNumber2 > randomNumber1)
+            resultTitle = 'Player 2 wins!';
+        else
+            resultTitle = 'It\'s a draw! Roll again.';
+
+        dice1.classList.remove("rolling");
+        dice2.classList.remove("rolling");
+
+        document.getElementById("heading").textContent = resultTitle;
+    }, duration);     
 });
 
 document.getElementById("dice1").setAttribute("src", "./images/dice" + randomNumber1 + ".png");
